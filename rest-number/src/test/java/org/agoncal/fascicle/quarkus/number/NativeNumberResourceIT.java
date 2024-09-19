1ac1c4c147e29a2a5a80e56aebd22c43ff295fcc
dddario <dariocabrera@tdf.edu.ar>
2024-09-19 13:48:50 -0300

    añadido rest-number

---
+++ rest-number/src/test/java/org/agoncal/fascicle/quarkus/number/NativeNumberResourceIT.java
@@ -0,0 +1,9 @@
+package org.agoncal.fascicle.quarkus.number;
+
+//import io.quarkus.test.junit.NativeImageTest;
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class NativeNumberResourceIT extends NumberResourceTest{
+
+}
---
+++ rest-number/src/test/java/org/agoncal/fascicle/quarkus/number/NumberResourceTest.java
@@ -0,0 +1,88 @@
+package org.agoncal.fascicle.quarkus.number;
+
+//import io.quarkus.test.junit.DisabledOnNativeImage;
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.ws.rs.core.HttpHeaders;
+import jakarta.ws.rs.core.MediaType;
+import org.junit.jupiter.api.Test;
+
+//import javax.ws.rs.core.HttpHeaders;
+//import javax.ws.rs.core.MediaType;
+
+import static io.restassured.RestAssured.given;
+//import static javax.ws.rs.core.Response.Status.NOT_FOUND;
+//import static javax.ws.rs.core.Response.Status.OK;
+//import static org.hamcrest.CoreMatchers.is;
+import static jakarta.ws.rs.core.Response.Status.OK;
+import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.not;
+
+//@formatter:off
+// tag::adocSnippet[]
+@QuarkusTest
+public class NumberResourceTest {
+
+
+  @Test
+  void shouldGenerateBookNumber() {
+    given()
+      .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON).
+      when()
+      .get("/api/numbers/book").
+      then()
+      .statusCode(OK.getStatusCode())
+      .body("$", hasKey("isbn_10"))
+      .body("$", hasKey("isbn_13"))
+      .body("$", hasKey("asin"))
+      .body("$", hasKey("ean_8"))
+      .body("$", hasKey("ean_13"))
+      .body("$", not(hasKey("generationDate")));
+  }
+
+  @Test
+  void shouldPingOpenAPI() {
+    given()
+      .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON).
+      when()
+      .get("/openapi").
+      then()
+      .statusCode(OK.getStatusCode());
+  }
+
+  @Test
+  void shouldPingSwaggerUI() {
+    given().
+      when()
+      .get("/swagger-ui").
+      then()
+      .statusCode(OK.getStatusCode());
+  }
+
+  @Test
+  void shouldPingLiveness() {
+    given().
+      when()
+      .get("/health/live").
+      then()
+      .statusCode(OK.getStatusCode());
+  }
+
+  @Test
+  void shouldPingReadiness() {
+    given().
+      when()
+      .get("/health/ready").
+      then()
+      .statusCode(OK.getStatusCode());
+  }
+  @Test
+  void shouldPingMetrics() {
+    given()
+      .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON).
+      when()
+      .get("/metrics/application").
+      then()
+      .statusCode(OK.getStatusCode());
+  }
+}
+// end::adocSnippet[]
